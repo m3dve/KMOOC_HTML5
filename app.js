c4bb79b9307e52ae3ada03b62067a9a44d25b8c4
m3dve <58778742+m3dve@users.noreply.github.com>
2020-06-02 14:58:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -22,13 +22,19 @@ var lineDirections = [
   [1, 1] //diagonal 2
 ];
 
-
+window.onload = function onInit() {
+  initScoreBoard();
+  getLocation();
+}
 
 function onClickStart(event) {
+  bJatekVege = false;
+  window.localStorage.lepesSzam = 0;
   event.preventDefault();
   window.localStorage.elsoJatekos = $("#choose").val();
   window.localStorage.masodikJatekos = $("#choose2").val();
   if (!window.localStorage.elsoJatekos || !window.localStorage.masodikJatekos) {
+    showMessage("A játékos neveket ne felejtsd el megadni!");
     return;
   }
   window.localStorage.nextPlaxer = true;
@@ -46,23 +52,29 @@ function onClickStart(event) {
     for (let j = 0; j < 5; j++) {
       board[i].push(0);
       let oCell = row.insertCell(j);
-      oCell.title = "[" + (i + 1) + "," + (5 - j) + "] cella";
+      oCell.title = "[" + (i + 1) + "," + (j+1) + "] cella";
       oCell.onclick = selectCell;
       oCell.className = "newCell";
       oCell.appendChild(addSvg(index[2]));
       oCell.i = i;
       oCell.j = j;
     }
-    $("td").tooltip();
+
   }
+  initScoreBoard();
+  $("#myTable td").tooltip();
 }
 
-function selectCell(event, init) {
+function selectCell(event) {
+  if(parseInt(window.localStorage.lepesSzam) === 25){
+    showMessage("Nincs lehetőség több lépésre!", true);
+    return;
+  }
   if (bJatekVege) {
     return;
   }
-  event.currentTarget
   if (!event.currentTarget.isSelected) {
+    window.localStorage.lepesSzam++;
     event.currentTarget.isSelected = true;
     event.currentTarget.removeChild(event.currentTarget.firstElementChild);
     event.currentTarget.appendChild(addSvg(bNextPlayer ? index[0] : index[1]));
@@ -80,12 +92,14 @@ function selectCell(event, init) {
     $("#dialog p").text("A kijelölt cella foglalt!")
     $("#dialog").dialog(dialogConfig);
   }
-  console.log(init);
+
 }
 
-function finishGame(player) {
-  $("#dialog p").text("A " + player + " játékos megnyerte a játszmát!");
+function finishGame(sPlayer) {
+  showMessage("A " + sPlayer + " játékos megnyerte a játszmát!", true);
   $("#dialog").dialog(dialogConfig);
+  getLocation();
+  updateScoreBoard(sPlayer);
 }
 
 function addSvg(sNextPlayer) {
@@ -94,6 +108,87 @@ function addSvg(sNextPlayer) {
   svg.classList.remove(svg.classList[0]);
   svg.classList.add("svgSymbol");
   return svg;
+}
+
+function showMessage(sMessage, bHaveConfig) {
+  $("#dialog p").text(sMessage);
+  if (bHaveConfig) {
+    $("#dialog").dialog(dialogConfig);
+  } else {
+    $("#dialog").dialog();
+  }
+}
+
+function updateScoreBoard(sPlayer) {
+  var table = document.getElementById("scoreTable"),
+    row = table.insertRow(table.rows.length),
+    oScoreTable = {},
+    oCell = row.insertCell(0);
+  oScoreTable.player = sPlayer;
+  oCell.innerHTML = sPlayer;
+
+  oCell = row.insertCell(1);
+
+  oCell.innerHTML = window.localStorage.lepesSzam;
+  oScoreTable.lepesSzam = window.localStorage.lepesSzam
+
+
+  oCell = row.insertCell(2);
+
+  oCell.innerHTML = window.localStorage.address;
+  oScoreTable.location = window.localStorage.address;
+
+  oCell = row.insertCell(3);
+
+  oCell.innerHTML = new Date();
+  oScoreTable.datum = new Date();
+
+  let aScoreTable = window.localStorage.aScoreTable === null ? [] : JSON.parse(window.localStorage.aScoreTable);
+  aScoreTable.push(oScoreTable);
+  window.localStorage.aScoreTable = JSON.stringify(aScoreTable);
+}
+
+function initScoreBoard() {
+
+  var table = document.getElementById("scoreTable");
+
+  while (table.rows.length !== 1) {
+    table.deleteRow(1);
+  }
+  let aScoreTable = JSON.parse(window.localStorage.aScoreTable);
+  aScoreTable = aScoreTable ? aScoreTable : [];
+  aScoreTable.forEach(element => {
+
+    let row = table.insertRow(table.rows.length);
+    let oCell = row.insertCell(0);
+    oCell.innerHTML = element.player;
+
+    oCell = row.insertCell(1);
+    oCell.innerHTML = element.lepesSzam;
+
+    oCell = row.insertCell(2);
+    oCell.innerHTML = element.location;
+
+    oCell = row.insertCell(3);
+    oCell.innerHTML = element.datum;
+
+  });
+}
+
+function getLocation() {
+
+  $.ajax({
+    url: "https://geolocation-db.com/jsonp",
+    jsonpCallback: "callback",
+    dataType: "jsonp",
+    success: function(location) {
+      window.localStorage.setItem("country", location.country_name);
+      window.localStorage.setItem("state", location.state);
+      window.localStorage.setItem("city", location.city);
+
+      window.localStorage.setItem("address", [window.localStorage.country, window.localStorage.state, window.localStorage.city].filter(x => typeof x === 'string' && x.length > 0 && x !== "null").join(","));
+    }
+  });
 }
 
 ////////////////////////////////////////
